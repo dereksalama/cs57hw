7fdd4de0b771fda55a98bd42e10049fabebb2b4d
Derek Salama <derek@derek-UX31E.(none)>
2013-04-04 15:46:29 -0400

    part 1 complete

--- hw1/lexdriver.c
+++ hw1/lexdriver.c
@@ -3,25 +3,14 @@
 extern char *yytext;
 extern int yyleng;
 
-#define OUTPUT_FILE_NAME "output.c"
 int main(void)
 {
     int token;
-    FILE *fp;
-    fp = fopen(OUTPUT_FILE_NAME, "w");
-    if (fp == NULL) {
-    	fprintf(stderr, "Can't open output file.\n");
-    	return 1;
+    while((token = yylex()) != EOFTOKEN) {
+        if (token != COMMENTTOKEN)
+            printf("%s", yytext);
     }
 
-    while ((token = yylex()) != EOFTOKEN) {
-        if (token <= 256)
-            printf("%c (%d): \"%s\" (%d)\n", token, token, yytext, yyleng);
-        else if (token == CODETOKEN) {
-        	fprintf(fp, "%s", yytext);
-        }
-    }
-
-    fclose(fp);
+    printf("%s", "\n");
     return 0;
 }
---
+++ hw1/test.c
@@ -0,0 +1,15 @@
+/*
+Multi line comment
+
+With lots of lines
+*
+
+
+*/
+
+int main(void) {
+	/*one line comment */
+	int bo = 2^2^2; /*inline comment */
+	int zo = /*mid-line comment8*/ bo;
+	return 0;
+}
