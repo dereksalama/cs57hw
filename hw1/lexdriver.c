0ad2531070f1bab05accc1b2bdee5377342f91fd
Derek Salama <derek@derek-UX31E.(none)>
2013-04-06 12:16:45 -0400

    refactor to print in .l

--- hw1/lexdriver.c
+++ hw1/lexdriver.c
@@ -7,26 +7,7 @@ extern int yyleng;
 int main(void)
 {
     int token;
-    long hexvalue;
-    while(1) {
-        token = yylex();
-        switch(token) {
-            case EOFTOKEN:
-                //print endline for nice formatting
-                printf("%s", "\n");
-                return 0;
-            case NUMTOKEN:
-                hexvalue = strtol(yytext, NULL, 0);
-                printf("%#lx", hexvalue);
-                break;
-            case COMMENTTOKEN:
-                break;
-            default:
-                printf("%s", yytext);
-                break;
-        }
-    }
-
-
+    while ((token = yylex()) != EOFTOKEN);
+    printf("\n");
     return 0;
 }
