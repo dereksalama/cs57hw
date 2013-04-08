653f5c047a7345d8e1a3be214c3ccbb0b2614526
Derek Salama <dereksalama@gmail.com>
2013-04-07 22:37:35 -0400

    try matching filled brackets first

--- hw1/test.c
+++ hw1/test.c
@@ -10,7 +10,7 @@ With lots of lines
 int main(void) {
 	/*one line comment */
 	int bo = 2^2^2; /*inline comment */
-	int zo = /*mid-line comment8*/ bo;
+	int zo = /*mid-line comment*/ bo; /* and again */
 
 	bo = 0x10;
 	bo = 16;
@@ -20,6 +20,9 @@ int main(void) {
 	array[bo[zo] + the[clown]];
 	array[/*with comment*/ 020];
 	array[/*this should just be "array[]"*/];
-	array[ /*even with */ /*multiple comments*/];
+	array[];
+	array[	];
+	array[
+			];
 	return 0;
 }
